1ba3feb8753c8e73dab318023ece882fd8119fa8
Jan Drozd Novak <jan.novak@outlook.com>
2023-10-31 10:22:49 +0000

    remove bad command

--- entry.sh
+++ entry.sh
@@ -3,6 +3,3 @@
 # Write the cron schedule and command to a file
 CRON_SCHEDULE="${CRON_SCHEDULE:-* * * * *}"
 crontab -l | { cat; echo "$CRON_SCHEDULE /usr/local/bin/find_and_backup.sh"; } | crontab -
-
-# Give the cron file appropriate permissions
-chmod 0644 /etc/cron.d/find_and_backup
