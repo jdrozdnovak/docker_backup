a56a288afa8c12c724910f792b913e53bc86d5dc
Jan Drozd Novak <jan.novak@outlook.com>
2023-10-31 12:27:15 +0000

    change default shell

--- entry.sh
+++ entry.sh
@@ -1,10 +1,10 @@
-#!/bin/bash
+#!/bin/sh
 
 # Write the cron schedule and command to a file
 echo "$CRON_SCHEDULE"
 echo $RCLONE_REMOTE_FOLDER
 echo $RCLONE_REMOTE_NAME
 echo $FAIL_NOTIFY_URL
-(crontab -l 2>/dev/null; printf "%s/bin/bash ./usr/local/bin/find_and_backup.sh >/proc/1/fd/1 2>/proc/1/fd/2\n" "$CRON_SCHEDULE") | crontab -
+(crontab -l 2>/dev/null; printf "%s /usr/local/bin/find_and_backup.sh >/proc/1/fd/1 2>/proc/1/fd/2\n" "$CRON_SCHEDULE") | crontab -
 
 cron -f
