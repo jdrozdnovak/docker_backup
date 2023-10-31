678c23f85ddfb28e36c05b5152190826b8fda8ff
Jan Drozd Novak <jan.novak@outlook.com>
2023-10-31 11:31:57 +0000

    fix redirect

--- entry.sh
+++ entry.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # Write the cron schedule and command to a file
-(crontab -l 2>/dev/null; printf "%s /usr/local/bin/find_and_backup.sh\n" "$CRON_SCHEDULE") | crontab -
+(crontab -l 2>/dev/null; printf "%s /usr/local/bin/find_and_backup.sh >/proc/1/fd/1 2>/proc/1/fd/2\n" "$CRON_SCHEDULE") | crontab -
 
 cron -f
