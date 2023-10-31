b9f619f57a2312292070238c89a2f096044259eb
Jan Drozd Novak <jan.novak@outlook.com>
2023-10-31 10:38:53 +0000

    fix default value

--- entry.sh
+++ entry.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 # Write the cron schedule and command to a file
-CRON_SCHEDULE="${CRON_SCHEDULE:-* * * * *}"
 crontab -l | { cat; echo "$CRON_SCHEDULE /usr/local/bin/find_and_backup.sh"; } | crontab -
