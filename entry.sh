ffcbc3f7ae98db664566d2d2f553ddc20eef2eb7
Jan Drozd Novak <jan.novak@outlook.com>
2023-10-31 11:14:06 +0000

    fix entry

--- entry.sh
+++ entry.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Write the cron schedule and command to a file
-(crontab -l 2>/dev/null; echo "$CRON_SCHEDULE /usr/local/bin/find_and_backup.sh") | crontab -
+(crontab -l 2>/dev/null; printf "%s /usr/local/bin/find_and_backup.sh\n" "$CRON_SCHEDULE") | crontab -
