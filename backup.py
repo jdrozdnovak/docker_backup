ede0418db6a307b6f14d8e090231b0a01566612d
Jan Drozd Novak <jan.novak@outlook.com>
2023-11-05 09:48:14 +0000

    refacrot python

--- backup.py
+++ backup.py
@@ -4,9 +4,9 @@ import yaml
 import subprocess
 import logging
 from zipfile import ZipFile
-from socket import gethostname
 import requests
 from datetime import datetime
+import socket
 
 logging.basicConfig(level=logging.INFO, stream=sys.stdout)
 logger = logging.getLogger(__name__)
@@ -20,6 +20,11 @@ class DockerBackup:
         self.remote_name = None
         self.fail_notify_url = None
         self.read_env_vars_from_file("/env_var")
+
+    @staticmethod
+    def normalize_path(path):
+        """Normalize a filesystem path."""
+        return os.path.normpath(os.path.abspath(path))
 
     def read_env_vars_from_file(self, file_path):
         try:
@@ -43,7 +48,6 @@ class DockerBackup:
         """Execute shell command."""
         try:
             logger.info(f"Running\n{command}")
-            command = command.replace(" ./", " ").replace("/./", "/")
             subprocess.run(
                 command, shell=True, check=True, stdout=sys.stdout, stderr=sys.stderr
             )
@@ -53,15 +57,14 @@ class DockerBackup:
             sys.exit(1)
 
     def notify_failure(self):
-        """Notify failure through a URL, ensuring no extra quotes."""
+        """Notify failure through a URL."""
         if self.fail_notify_url:
-            sanitized_url = self.fail_notify_url.strip('"').strip("'")
             try:
                 requests.post(
-                    sanitized_url, json={"message": "Backup process failed."}
+                    self.fail_notify_url, json={"message": "Backup process failed."}
                 )
             except Exception as exc:
-                logger.error(f"Failed to notify via URL {sanitized_url}: {exc}")
+                logger.error(f"Failed to notify via URL {self.fail_notify_url}: {exc}")
 
     def validate_and_notify_env_vars(self):
         """Validate and notify for environment variables."""
@@ -92,23 +95,19 @@ class DockerBackup:
             logger.error(f"Failed to read docker-compose file: {e}")
             sys.exit(1)
 
-    def get_real_volume_names(self, volume_names):
+    def get_real_volume_names(self, compose_data, base_dir):
         real_volume_names = []
-        for volume_name in volume_names:
-            # Check if the volume name is a path and convert it to an absolute path if it is relative
-            if '/' in volume_name:
-                volume_name = os.path.abspath(volume_name)
-            # Now proceed to check if this volume exists
-            try:
-                cmd_output = subprocess.getoutput(
-                    f"docker volume ls --filter name=^{volume_name}$"
-                )
-                if cmd_output:
+        for service_name, data in compose_data.get("services", {}).items():
+            if service_name == "docker-backup":
+                continue
+            for volume in data.get("volumes", []):
+                volume_name = volume.split(":")[0]
+                if os.path.isabs(volume_name) or volume_name.startswith("."):
+                    volume_name = self.normalize_path(
+                        os.path.join(base_dir, volume_name)
+                    )
+                if volume_name not in real_volume_names:
                     real_volume_names.append(volume_name)
-            except Exception as e:
-                self.notify_failure()
-                logger.error(f"Failed to fetch docker volumes: {e}")
-                sys.exit(1)
         return real_volume_names
 
     def backup_volumes_to_zip(self, base_dir, real_volume_names):
@@ -119,10 +118,7 @@ class DockerBackup:
                     full_path = os.path.join(root, file)
                     if full_path == zip_file_path:
                         continue
-                    zipf.write(
-                        full_path,
-                        os.path.relpath(full_path, base_dir),
-                    )
+                    zipf.write(full_path, os.path.relpath(full_path, base_dir))
         return zip_file_path
 
     def rclone_upload(self, file_path, parent_folder_name, suffix):
@@ -135,57 +131,44 @@ class DockerBackup:
             f"rclone sync --progress {file_path} {remote_path} --backup-dir {remote_old_path} --suffix {suffix} --suffix-keep-extension"
         )
 
-    def main(self, docker_compose_file):
+    def get_hostname() -> str:
+        """Retrieve the hostname."""
         try:
-            suffix = datetime.now().strftime("%y%m%d%H%M")
-            base_dir = os.path.dirname(docker_compose_file)
-            parent_folder_name = os.path.basename(base_dir)
+            with open("/etc/host_hostname", "r") as file:
+                hostname = file.read().strip()
+        except FileNotFoundError:
+            hostname = socket.gethostname()
+        return hostname
 
-            compose_data = self.read_docker_compose(docker_compose_file)
+    def main(self, docker_compose_file):
+        base_dir = os.path.dirname(docker_compose_file)
+        compose_data = self.read_docker_compose(docker_compose_file)
+        real_volume_names = self.get_real_volume_names(compose_data, base_dir)
 
-            volume_names = []
-            for service_name, data in compose_data.get("services", {}).items():
-                if service_name == "docker-backup":
-                    continue
-                for volume in data.get("volumes", []):
-                    volume_name, _, _ = volume.partition(":")
-                    if os.path.isabs(volume_name):
-                        continue
-                    if volume_name not in volume_names:
-                        volume_names.append(volume_name)
+        for real_volume_name in real_volume_names:
+            volume_dir = self.normalize_path(f"{base_dir}/{real_volume_name}_backup")
+            os.makedirs(volume_dir, exist_ok=True)
+            volume_backup_path = (
+                f"{volume_dir}/{os.path.basename(real_volume_name)}.tar.gz"
+            )
+            self.execute_command(
+                f"docker run --rm --volume {real_volume_name}:/backup --volume {volume_dir}:/backup_dir ubuntu tar czfP {volume_backup_path} -C / /backup/"
+            )
 
-            real_volume_names = self.get_real_volume_names(volume_names)
-
-            for real_volume_name in real_volume_names:
-                volume_dir = f"{base_dir}/{real_volume_name}_backup"
-                os.makedirs(volume_dir, exist_ok=True)
-                self.execute_command(
-                    f"docker run --rm --volume {real_volume_name}:/backup --volume {volume_dir}:/backup_dir ubuntu tar czfP /backup_dir/{real_volume_name}.tar.gz -C / /backup/"
-                )
-
-            zip_file_path = self.backup_volumes_to_zip(base_dir, real_volume_names)
-
-            self.rclone_upload(zip_file_path, parent_folder_name, suffix)
-
-            self.cleanup(base_dir, volume_names)
-            logging.info("Backup completed successfully.")
-        except Exception as e:
-            logger.error(f"Backup process failed: {e}")
-            self.notify_failure()
-            sys.exit(1)
-
-
-def get_hostname() -> str:
-    """Retrieve the hostname."""
-    try:
-        with open("/etc/host_hostname", "r") as file:
-            hostname = file.read().strip()
-    except FileNotFoundError:
-        hostname = gethostname()
-    return hostname
+        backup_zip_path = self.backup_volumes_to_zip(base_dir, real_volume_names)
+        self.rclone_upload(
+            backup_zip_path,
+            os.path.basename(base_dir),
+            datetime.now().strftime("%Y%m%d%H%M%S"),
+        )
+        self.cleanup(base_dir, real_volume_names)
 
 
 if __name__ == "__main__":
-    DOCKER_COMPOSE_FILE = sys.argv[1]
-    BACKUP = DockerBackup()
-    BACKUP.main(DOCKER_COMPOSE_FILE)
+    if len(sys.argv) != 2:
+        sys.stderr.write("Usage: backup.py <path_to_docker_compose_file>\n")
+        sys.exit(1)
+
+    docker_compose_file = sys.argv[1]
+    backup = DockerBackup()
+    backup.main(docker_compose_file)
--- find_and_backup.sh
+++ find_and_backup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Search for docker-compose files and perform backup
-find /docker/ -name "docker-compose*.yml" -exec python3.11 /usr/local/bin/backup.py {} \;
+find /docker/ -type f \( -name "docker-compose*.yml" -o -name "docker-compose*.yaml" \) -exec python3.11 /usr/local/bin/backup.py {} \;
