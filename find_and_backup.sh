f88d28d0d391888d1d897c472ba6167c6094cd88
jdnovak <jan.novak@outlook.com>
2024-10-02 13:18:59 +0200

    update

--- find_and_backup.sh
+++ find_and_backup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
+PYTHON_EXEC="/usr/src/app/.venv/bin/python"
 # Search for docker-compose files and perform backup
-find /docker/ -type f \( -name "docker-compose*.yml" -o -name "docker-compose*.yaml" \) -exec python3.12 /usr/local/bin/backup.py {} \;
+find /docker/ -type f \( -name "docker-compose*.yml" -o -name "docker-compose*.yaml" \) -exec $PYTHON_EXEC /usr/local/bin/backup.py {} \;
