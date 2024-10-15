dee8d07c2e52d5ea1f5d6aff0eee42b761751741
jdnovak <jan.novak@outlook.com>
2024-10-15 13:45:26 +0200

    one more try

--- find_and_backup.sh
+++ find_and_backup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Search for docker-compose files and perform backup
-find /docker/ -type f \( -name "docker-compose*.yml" -o -name "docker-compose*.yaml" \) -exec python3 /usr/local/bin/backup.py {} \;
+find /docker/ -type f \( -name "docker-compose*.yml" -o -name "docker-compose*.yaml" \) -exec python /usr/local/bin/backup.py {} \;
